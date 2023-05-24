71f0123ca952120f9af7f5513026b07b6e0020c0
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-24 19:04:41 +0530

    [welcome Regex problem]

---
+++ RegularExpression/RegularExpression/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Security;
+
+namespace RegularExpression
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("hello regex problem");
+        }
+    }
+}
